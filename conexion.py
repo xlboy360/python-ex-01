04a5808fc32ac75f48c48e60c2c5d6360acfe29f
Juan Salgado <juan_jsc360@hotmail.com>
2020-08-15 20:42:53 -0500

    Uso comillas simples
    Se corrigió el uso de comillas dobles por simples

--- conexion.py
+++ conexion.py
@@ -2,11 +2,11 @@ import psycopg2 as db
 
 class Conexion:
     #Elementos de la Conexion
-    __DATABASE = "own_tests"
-    __USERNAME = "postgres"
-    __PASSWORD = "admin"
-    __DB_PORT = "5432"
-    __HOST = "123.0.0.1"
+    __DATABASE = 'own_tests'
+    __USERNAME = 'postgres'
+    __PASSWORD = 'admin'
+    __DB_PORT = '5432'
+    __HOST = '123.0.0.1'
     __conexion = None
     __cursor = None
 
@@ -20,7 +20,7 @@ class Conexion:
                                             port=cls.__DB_PORT,
                                             database=cls.__DATABASE)
             except Exception as e:
-                print(f"Error al conectar a la base de datos. {e}")
+                print(f'Error al conectar a la base de datos. {e}')
         else:
             return cls.__conexion
 
@@ -31,7 +31,7 @@ class Conexion:
                 cls.__cursor = cls.obtenerConexion().cursor()
                 return cls.__cursor
             except Exception as e:
-                print(f"No se ha podido establecer el cursor: {e}")
+                print(f'No se ha podido establecer el cursor: {e}')
         else:
             return cls.__cursor
 
@@ -41,10 +41,10 @@ class Conexion:
             try:
                 cls.__cursor.close()
             except Exception as e:
-                print(f"No se ha podido cerrar el cursor: {e}")
+                print(f'No se ha podido cerrar el cursor: {e}')
         if cls.__conexion is not None:
             try:
                 cls.__conexion.close()
             except Exception as e:
-                print(f"No se ha podido cerrar la conexión: {e}")
-        print("Se han cerrado con éxito!")
+                print(f'No se ha podido cerrar la conexión: {e}')
+        print('Se han cerrado con éxito!')
